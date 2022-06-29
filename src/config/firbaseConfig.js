510b99e85d90feb332f56c38976dace15a0521a3
Rankit <76683305+rankit2001@users.noreply.github.com>
2022-06-29 15:39:41 +0530

    Delete src directory

--- src/config/firbaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCUFImC1mt4ifUj1DJmLwMktYefF9fRW2U",
-  authDomain: "crypto-hunter-react.firebaseapp.com",
-  projectId: "crypto-hunter-react",
-  storageBucket: "crypto-hunter-react.appspot.com",
-  messagingSenderId: "967857528174",
-  appId: "1:967857528174:web:251523693fb88ca1d4757f"
-};
-export default firebaseConfig;
